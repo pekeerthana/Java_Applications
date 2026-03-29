4711c613f367d0c10e34a1a4dd1299f86ff46cc8
pekeerthana <keerthanarajavardhan@gmail.com>
2026-03-29 22:47:01 +0530

    added test profile in service integration test

--- demo/src/test/java/com/example/demo/service/UserServiceIntegrationTest.java
+++ demo/src/test/java/com/example/demo/service/UserServiceIntegrationTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 import com.example.demo.dto.UserRequestDTO;
 import com.example.demo.entity.User;
@@ -15,7 +16,9 @@ import com.example.demo.exception.DuplicateEmailException;
 import com.example.demo.exception.UserNotFoundException;
 import com.example.demo.respository.UserRepository;
 
+
 @SpringBootTest
+@ActiveProfiles("test")
 @Transactional
 public class UserServiceIntegrationTest {
 
